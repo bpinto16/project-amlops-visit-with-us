b6b1850b32137564ca5972dbc872ee6208e27e4c
Bryan Pinto <bryan.pinto16@gmail.com>
2026-05-23 19:25:42 +0000

    Push from collab.

--- tourism_project/hosting/hosting.py
+++ tourism_project/hosting/hosting.py
@@ -8,6 +8,6 @@ api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="tourism_project/deployment",     # the local folder containing your files
     repo_id=HF_MODEL_REPO,          # the target repo
-    repo_type="space",                      # dataset, model, or space
+    repo_type="model",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
